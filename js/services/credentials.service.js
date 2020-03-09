6fb51a496622c930bd07c29755018a102f5d716b
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-03-09 17:02:40 +0000

    Update credentials.service.js

--- js/services/credentials.service.js
+++ js/services/credentials.service.js
@@ -14,8 +14,8 @@
         var service = {
             clientId: "92e06e65050a4c64be6bfe39ec3fde21",
             redirectShort: "https://tee-emm97.github.io/little_spot_spwa/callback",
-            redirectUri: "https://tee-emm97.github.io/little_spot_spwa/callback",
-            internalRedirectUri: "http://localhost:5500/#/auth_secure",
+            redirectUri: "https://tee-emm97.github.io/little_spot_spwa/callback.html",
+            internalRedirectUri: "https://tee-emm97.github.io/little_spot_spwa/auth_secure",
             scopes: "streaming, user-read-playback-state, user-modify-playback-state",
             authServiceUri: "https://accounts.spotify.com/authorize",
             exchangeServiceUri: "http://localhost:8000/spotify/exchange",
