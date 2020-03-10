24df8cb7b1e2e65c3155bbe12a9d9be618729e76
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-03-10 12:57:40 +0000

    Update credentials.service.js

--- js/services/credentials.service.js
+++ js/services/credentials.service.js
@@ -14,7 +14,7 @@
         var service = {
             clientId: "92e06e65050a4c64be6bfe39ec3fde21",
             redirectShort: "https://littlespotspwa.herokuapp.com/little_spot_spwa/callback",
-            redirectUri: "https://littlespotspwa.herokuapp.com/little_spot_spwa/callback.html",
+            redirectUri: "https://littlespotspwa.herokuapp.com/little_spot_spwa/callback",
             internalRedirectUri: "https://littlespotspwa.herokuapp.com/little_spot_spwa/js/states/secure-auth/authenticate.secure.html",
             scopes: "streaming, user-read-playback-state, user-modify-playback-state",
             authServiceUri: "https://accounts.spotify.com/authorize",
