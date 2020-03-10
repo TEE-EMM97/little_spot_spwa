b09a166493f48a8fe2a1c7485e2d3051e0329d94
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-03-10 13:35:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const app = express();
 // Is this still needed?
 app.use(express.static(path.join(__dirname, '')));
 
-app.get('/*', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname + req.url));
 });
 
@@ -58,7 +58,7 @@ app.get('/*', (req, res, next) => {
 //Base href Adds the little spot path again
 //We remove it using the split function
 //This allows for full deployment on heroku
-app.get('/little_spot_spwa/', (req, res, next) => {
+app.get('/little_spot_spwa/*', (req, res, next) => {
   res.sendFile(path.join(__dirname + req.url.split("little_spot_spwa")[1]));
 });
 
